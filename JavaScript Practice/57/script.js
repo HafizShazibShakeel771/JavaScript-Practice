d92443dc07e0585abc21f16b49a522df5dd5e0ac
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-01 19:06:28 -0800

    commit

--- JavaScript Practice/57/script.js
+++ JavaScript Practice/57/script.js
@@ -1,1 +1,38 @@
 console.log("This is a Tutorial Loop");
+// This is a simple loop that prints numbers from 1 to 100
+/*
+let a = 1;
+ for (let i = 0; i < 100; i++) {
+    console.log(a + i);
+ }
+
+ let obj = {
+    name: "John",
+    role: "Developer",
+    company: "Tech Solutions",
+ }*/
+
+// This is an object with properties
+/*
+for (const key in obj) {
+    const element = obj[key];
+        console.log(key, element);  
+    }
+
+     for (const key in obj) {
+    const element = key;
+        console.log(key);  
+    }
+*/
+// This is a loop that prints each character of a string 
+/*  
+for (const char of "Shazib") {
+        console.log(char);
+    }
+*/
+// This is a while loop that prints numbers from 5 to 9
+let i = 5;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
