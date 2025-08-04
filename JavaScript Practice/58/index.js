8eb3aa6a43ea298e0dd419f7c2f1cbe67edc5194
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-04 12:19:19 -0800

    commit

---
+++ JavaScript Practice/58/index.js
@@ -0,0 +1,9 @@
+
+function nice(name){
+    console.log("Hello, " + name + "you are nice!");
+    console.log("Hello, " + name + "you nice!");
+    console.log("Hello, " + name + "you are good nice!");
+    console.log("Hello, " + name + "you course is good to nice!");
+}
+
+nice("John");
