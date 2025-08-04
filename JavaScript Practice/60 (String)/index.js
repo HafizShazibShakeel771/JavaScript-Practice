bb55582bd61de01304b61a6a615d3082a54c306d
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-04 15:29:15 -0800

    commit

---
+++ JavaScript Practice/60 (String)/index.js
@@ -0,0 +1,18 @@
+console.log("This is String Tutorials");
+
+let a = "Shazib";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+//console.log(a[6]); // undefined, as there is no 7th character
+
+console.log(a.length);
+
+let real_name = "Shazib";
+let friend = "Ali";
+
+console.log("His name is " + real_name + " his a friend name is " + friend);
+console.log(`His name is ${real_name} his a friend name is ${friend}`); // Template Literals
