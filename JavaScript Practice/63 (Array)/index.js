08bd6532919d11af041df711a4cc15e9d3490509
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-05 12:29:48 -0800

    Update index.js

--- JavaScript Practice/63 (Array)/index.js
+++ JavaScript Practice/63 (Array)/index.js
@@ -1,7 +1,6 @@
 let arr = [1, 2, 4, 5, 7];
 // Adding elements to the array
 arr[0] = 5666; // Change the first element to 5666
-
 /*
 console.log(arr, typeof arr); // Output: [5666, 2, 4, 5, 7] 'object'
 console.log(arr.length); // Output: 7
@@ -14,4 +13,4 @@ console.log(arr[4]); // Output: 10
 */
 
 console.log(arr.toString()); // Output: "5666,2,4,5,7"
-
+console.log(arr.join(" | ")); // Output: "5666 | 2 | 4 | 5 | 7"
