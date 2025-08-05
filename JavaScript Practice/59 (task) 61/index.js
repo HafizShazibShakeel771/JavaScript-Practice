1c272fe1bf097dc4ab8a6ea0fc3ce83266b966f1
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-04 16:13:55 -0800

    commit

---
+++ JavaScript Practice/59 (task) 61/index.js
@@ -0,0 +1,23 @@
+let random = Math.random();
+let a = prompt("Enter first number");
+let b = prompt("Enter second number");
+let c = prompt("Enter operation");
+
+let obj = {
+"+":"-",
+"-":"/",
+"*":"+",
+"/":"**",
+}
+
+if (random > 0.1) {
+    //perform correct calculation
+    alert(`This is the correct calculation: ${eval$(`${a} ${c} ${b}`)}`);
+
+}
+
+else {
+    //perform incorrect calculation}
+    c = obj[c];
+
+}
