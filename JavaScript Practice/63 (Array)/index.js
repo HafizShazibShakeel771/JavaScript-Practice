5a74db16f10e391c29d6e0b7ceda7597f1cab42f
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-05 12:53:18 -0800

    Update index.js

--- JavaScript Practice/63 (Array)/index.js
+++ JavaScript Practice/63 (Array)/index.js
@@ -11,6 +11,45 @@ console.log(arr[0]); // Output: 100
 console.log(arr[2]); // Output: 2
 console.log(arr[4]); // Output: 10
 */
-
+// Accessing elements in the array
 console.log(arr.toString()); // Output: "5666,2,4,5,7"
 console.log(arr.join(" | ")); // Output: "5666 | 2 | 4 | 5 | 7"
+// Converting to string
+
+// Removing elements from the array
+let n = [1, 2, 3, 4];
+console.log(n); // Output: [1, 2, 3, 4]
+n.pop(); // Removes the last element (4)
+console.log(n); // Output: [1, 2, 3]
+
+// Adding elements to the array
+n.push(5); // Adds 5 to the end of the array
+console.log(n); // Output: [1, 2, 3, 5]
+
+// Using shift() to remove the first element
+n.shift(); // Removes the first element (1)
+console.log(n); // Output: [2, 3, 5]
+
+// Using unshift() to add an element at the beginning
+n.unshift(8); // Adds 0 to the beginning of the array
+console.log(n); // Output: [8, 2, 3, 5]
+
+// Deleting an element using delete
+delete n[0]; // Deletes the first element (8)
+console.log(n); // Output: [ <1 empty item>, 2, 3, 5 ]
+console.log(n.length); // Output: 4
+
+/*
+// Concatenating arrays
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let arr3 = [7, 8, 9];
+let arr4 = arr1.concat(arr2, arr3); // Concatenates arr1 and arr2
+console.log(arr4); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+*/
+
+// Sorting an array
+let a = [9, 8, 6];
+a.sort(); // Sorts the array in ascending order
+console.log(a); // Output: [6, 8, 9]
+
