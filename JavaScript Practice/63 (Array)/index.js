60b3935527d05b4a6fbf140ffd6ee81ec25d3582
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-05 13:46:59 -0800

    Update index.js

--- JavaScript Practice/63 (Array)/index.js
+++ JavaScript Practice/63 (Array)/index.js
@@ -50,6 +50,37 @@ console.log(arr4); // Output: [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 // Sorting an array
 let a = [9, 8, 6];
+console.log(a); // Output: [9, 8, 6]
 a.sort(); // Sorts the array in ascending order
 console.log(a); // Output: [6, 8, 9]
+
+// Reversing an array
+let b = [1, 2, 3, 4, 5];
+console.log(b); // Output: [1, 2, 3, 4, 5]
+b.reverse(); // Reverses the order of elements in the array
+console.log(b); // Output: [5, 4, 3, 2, 1]
+
+//splice() method
+let c = [1, 2, 3, 4, 5];
+console.log(c); // Output: [1, 2, 3, 4, 5]
+c.splice(1, 2); // Removes 2 elements starting from index 1
+console.log(c); // Output: [1, 4, 5]
+
+// Splicing to add elements
+console.log(c); // Output: [1, 4, 5]
+c.splice(2, 1, 2); // Adds 2 at index 2 and removes 1 element
+console.log(c); // Output: [1, 4, 2, 5]
+
+// Splicing to add multiple elements
+c.splice(1, 0, 3, 6); // Adds 3 and
+// 6 at index 1 without removing any elements
+console.log(c); // Output: [1, 3, 6, 4, 2, 5]
+
+
+// slice() method
+let d = [1, 2, 3, 4, 5];
+console.log(d); // Output: [1, 2, 3, 4, 5];
+// Slicing the array
+let e = d.slice(1, 3); // Creates a new array with elements from index 1 to 2
+console.log(e); // Output: [2, 3]
 
