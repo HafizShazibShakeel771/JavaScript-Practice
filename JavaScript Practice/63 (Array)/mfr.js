e2d051c500d0bd3a0dcabb15193a140fcd9aca47
HafizShazibShakeel771 <hafizshazibshakeel771@gmail.com>
2025-08-11 17:20:46 -0800

    Update mfr.js

--- JavaScript Practice/63 (Array)/mfr.js
+++ JavaScript Practice/63 (Array)/mfr.js
@@ -8,7 +8,7 @@ for (let index = 0; index < arr.length; index++) {
     newArr.push(element * 2);
     
 }
-console.log(newArr); // Output: [2, 26, 10, 14, 22]
+console.log(newArr); // Output: [2, 26, 10, 14, 22] 
 */
 
 /* Using forEach method */
@@ -19,4 +19,20 @@ let newArr = arr.map((e,index, arr)=>{
 });
 console.log(newArr); // Output: [2, 26, 10, 14, 22]
 
+const greaterThanSeven = (e)=>{
+    if (e>7) {
+        return true;
+    }
+    return false;
+}
+console.log(arr.filter(greaterThanSeven));
+// Output: [13, 11]
+
+// Using reduce method
+let arr2 = [1, 2, 3, 4, 5, 6];
+
+const red = (a, b)=>{
+    return a + b;
+}
+console.log(arr2.reduce(red)); // Output: 21
 
